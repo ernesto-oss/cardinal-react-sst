7685fcd7a342d88816535af59b40ec45069a407c
Ernesto Resende <dev@ernestoresende.com>
2023-03-09 20:24:16 -0300

    feat: replicated template from cardinal-next

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { request } from "graphql-request";
+import { useQuery } from "@tanstack/react-query";
+import { graphql } from "@/generated/graphql";
+import { getGraphqlUrl } from "@/utils/getBaseUrl";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Layout } from "@/layouts/layout";
+import { Hero } from "@/components/hero";
+import { DocsCard } from "@/components/docs-card";
+
+const greetingsQuery = graphql(`
+  query Greetings {
+    greeting
+  }
+`);
+
+const App: React.FC = () => {
+  const { data: query, isLoading: queryLoading } = useQuery({
+    queryKey: ["greetings"],
+    queryFn: async () => request(getGraphqlUrl(), greetingsQuery),
+  });
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Layout>
+      <Hero />
+      <div className="flex w-full flex-col items-center justify-between">
+        <div className="flex max-w-5xl flex-col items-center justify-center px-6">
+          <div className="mb-24 w-fit rounded-md border-2 border-slate-200/5 bg-slate-400/10 px-10 py-2 text-center">
+            {/* {queryLoading && (
+              <p className="font-mono font-semibold text-slate-300">Loading query</p>
+            )}
+            {query && <p className="font-mono font-semibold text-slate-300">{query.greeting}</p>} */}
+            <p className="font-mono font-semibold text-slate-300">Hello from GraphQL</p>
+          </div>
+          <div className="grid grid-cols-3 gap-6">
+            <DocsCard
+              href=""
+              title="First Steps"
+              description="The bare minimum you will need to setup database, authentication and deployment for your application."
+            />
+            <DocsCard
+              href=""
+              title="Documentation"
+              description="Find in-depth information about the tech stack and recommended setups for the libraries in use."
+            />
+            <DocsCard
+              href=""
+              title="Deployment"
+              description="Learn how use the provided deployment paths based on your stack."
+            />
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
-}
+    </Layout>
+  );
+};
 
-export default App
+export default App;
