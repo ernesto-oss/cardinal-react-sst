fab9c0e30ab905b381cdbea3d8e4afc05b2f6192
Ernesto Resende <dev@ernestoresende.com>
2023-03-09 20:17:22 -0300

    chore: remove web-next

--- apps/web-next/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  extends: ["../../.eslintrc.cjs", "next"],
-  /* Ignore artifacts from codegen */
-  ignorePatterns: ["fragment-masking.ts"],
-};
